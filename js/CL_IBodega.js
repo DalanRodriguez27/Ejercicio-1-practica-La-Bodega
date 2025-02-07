3f383126f815adb43c221fc72a534155a32ab207
Dalan <dalancho2411@gmail.com>
2025-02-06 23:15:54 -0400

    confirmacion

--- js/CL_IBodega.js
+++
@@ -1,8 +0,0 @@
-export default class CL_IBodega{
-
-    reporteBodega(t){
-        return`
-        <br>Reporte de la bodega
-        <br>Ganancia total del lote de articulos: ${t} `
-    }
-}
--- js/principal.js
+++ js/principal.js
@@ -1,23 +1,22 @@
 import CL_Articulo from "./CL_Articulo.js";
 import CL_Bodega from "./CL_Bodega.js";
-import CL_IArticulo from "./CL_IArticulo.js";
-import CL_IBodega from "./CL_IBodega.js";
-
-let iBodega = new CL_IBodega(),
-bodega = new CL_Bodega(),
-salida = document.getElementById("salida");
 
 
-let opc = 1;
+let articulo1 = new CL_Articulo(10);
+let articulo2 = new CL_Articulo(20);
+let articulo3 = new CL_Articulo(14);
+let articulo4 = new CL_Articulo(6);
+let articulo5 = new CL_Articulo(4);
 
-while(opc == 1){
-    let iArticulo = new CL_IArticulo(),
-    c = iArticulo.leerCosto(),
-    articulo = new CL_Articulo(c);
+let bodega = new CL_Bodega();
 
-    bodega.procesarArticulo(articulo);
+bodega.procesarArticulo(articulo1);
+bodega.procesarArticulo(articulo2); 
+bodega.procesarArticulo(articulo3);
+bodega.procesarArticulo(articulo4);
+bodega.procesarArticulo(articulo5);
 
-    opc = iArticulo.solicitarOpcion();
-}
+let salida = document.getElementById("salida");
 
-salida.innerHTML = iBodega.reporteBodega(bodega.totalGanancia());
+salida.innerHTML =`
+<br>El total de ganacia es de: ${bodega.totalGanancia()}`
